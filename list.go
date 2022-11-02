56ac0e8444bd8a9a6b4f02d97d623c1b036c74b7
eddiesalvador <71903910+eddiesalvador@users.noreply.github.com>
2022-11-02 18:56:13 -0400

    attempting to put a test package in

--- list.go
+++ list.go
@@ -68,24 +68,5 @@ func (l *List) Reverse() {
 	Display(l.head)
 }
 
-func test() {
-	fmt.Print("Hello Binge-path!")
-	// link := List{}
-	// link.Insert(5)
-	// link.Insert(9)
-	// link.Insert(13)
-	// link.Insert(22)
-	// link.Insert(28)
-	// link.Insert(36)
-	
-	// fmt.Println("\n==============================\n")
-	// fmt.Printf("Head: %v\n", link.head.key)
-	// fmt.Printf("Tail: %v\n", link.tail.key)
-	// link.Display()
-	// fmt.Println("\n==============================\n")
-	// fmt.Printf("head: %v\n", link.head.key)
-	// fmt.Printf("tail: %v\n", link.tail.key)
-	// link.Reverse()
-	// fmt.Println("\n==============================\n")
-}
+
 
